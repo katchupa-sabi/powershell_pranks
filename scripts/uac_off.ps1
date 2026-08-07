7c366a4245e4a05bc623c48d3d735dfaca009c77
Miguel Santos <zcbmqtuo00@gmail.com>
2026-08-07 15:50:52 +0100

    .

--- scripts/uac_off.ps1
+++ scripts/uac_off.ps1
@@ -1,2 +1,7 @@
+# Desativa o UAC (User Account Control) no Windows
+$Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance"
+New-Item -Path $Path -Force | Out-Null
+New-ItemProperty -Path $Path -Name "Enabled" -PropertyType DWord -Value 0 -Force | Out-Null
+Start-Sleep -Seconds 5
 New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
-Start-Process -FilePath "shutdown.exe" -ArgumentList "/r /t 0" -WindowStyle Hidden
+#Start-Process -FilePath "shutdown.exe" -ArgumentList "/r /t 0" -WindowStyle Hidden
